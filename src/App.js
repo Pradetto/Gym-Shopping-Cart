0aa03d8161e0f9ff5dd3c56110b9dcea4aceeca4
Michael Pradetto <94888847+Pradetto@users.noreply.github.com>
2023-03-02 13:56:05 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ function App() {
               <Route path="/login" element={<LoginPage />} />
               <Route path="/checkout" element={<CheckoutPage />} />
               <Route path="/favorites" element={<FavoritesPage />} />
+              <Route path="*" element={<Navigate to="/home" replace />} />
             </Routes>
           </Suspense>
         </GridItem>
